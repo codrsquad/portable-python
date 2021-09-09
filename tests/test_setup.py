3c46ad3399a93fec328ec063c36a7e47d8331b0a
Zoran Simic <zsimic@netflix.com>
2021-09-09 12:37:01 -0700

    More tests and comments

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -7,8 +7,16 @@ def test_config(cli, monkeypatch):
     cli.run("--dryrun", "-c", cli.tests_path("sample-config1.yml"), "build", "3.9.7", "-mnone")
     assert cli.succeeded
     assert "env MACOSX_DEPLOYMENT_TARGET=11" in cli.logged  # Comes from more specific macos-arm64.yml
-    assert " -> dist/cpython-3.9.7-macos-arm64.zip" in cli.logged  # Comes from macos.yml (not defined in macos-arm64.yml)
+    assert " -> dist/cpython-3.9.7-macos-arm64.tar.xz" in cli.logged  # Comes from macos.yml (not defined in macos-arm64.yml)
     assert "Would run: build/cpython-3.9.7/3.9.7/bin/python -mpip install -U wheel" in cli.logged
+    assert "--enable-optimizations" in cli.logged  # From default config
+
+    monkeypatch.setenv("PP_TARGET", "linux-x86_64")
+    cli.run("--dryrun", "-c", cli.tests_path("sample-config1.yml"), "build", "3.9.7", "-mnone")
+    assert cli.succeeded
+    assert "env MACOSX_DEPLOYMENT_TARGET" not in cli.logged
+    assert " -> dist/cpython-3.9.7-linux-x86_64.tar.gz" in cli.logged  # Default .tar.gz
+    assert "pip install -U wheel" not in cli.logged  # No pip-install section for linux
     assert "--enable-optimizations" in cli.logged  # From default config
 
 
