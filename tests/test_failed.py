b723306b83d7d11657468c10a228a9707ce02136
Zoran Simic <zsimic@netflix.com>
2021-10-06 09:30:21 -0700

    Less specific test

--- tests/test_failed.py
+++ tests/test_failed.py
@@ -7,7 +7,7 @@ def test_build_bogus_platform(cli):
 def test_invalid(cli):
     cli.run("-n", "build", "foo")
     assert cli.failed
-    assert "Invalid python spec: ?foo" in cli.logged
+    assert "Invalid python spec" in cli.logged
 
     cli.run("-n", "build", "3.6")
     assert cli.failed
