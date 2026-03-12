35f4552b9febf33fcb34b462575efa13cc82483a
Tim Hatch <timhatch@netflix.com>
2026-03-12 13:00:37 -0700

    Bump lowest supported python

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Zoran Simic zoran@simicweb.com",
     keywords="python, portable, binary",
     url="https://github.com/codrsquad/portable-python",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     entry_points={
         "console_scripts": [
             "portable-python = portable_python.__main__:main",
@@ -22,11 +22,11 @@ setup(
         "Operating System :: Unix",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Build Tools",
         "Topic :: System :: Installation/Setup",
