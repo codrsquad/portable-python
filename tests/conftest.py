5e63d6225d9fb0ae0cfebe252749508d6f0a07c2
Zoran Simic <zsimic@netflix.com>
2026-03-18 14:16:34 -0700

    Updated requirements constraints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,15 @@
 import runez
-from runez.conftest import cli, logged, temp_folder
+from runez.conftest import cli, ClickRunner, logged, temp_folder  # noqa: F401, fixtures
 from runez.http import GlobalHttpCalls
 
 from portable_python.cli import main
 
 GlobalHttpCalls.forbid()
 
-# These are fixtures, satisfying linters with an assert
-assert logged
-assert temp_folder
-
 # Ensure common logging setup is done throughout all tests (even tests not invoking cli)
 runez.log.setup(debug=True, console_format="%(levelname)s %(message)s", locations=None)
 
-cli.default_main = main
+ClickRunner.default_main = main
 
 
 def dummy_tarball(folders, basename, content=None):
